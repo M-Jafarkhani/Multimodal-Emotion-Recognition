600af54107bacd793a7b9355a32f91dd9d4a1003
M-Jafarkhani <mahdi.jafarkhani@gmail.com>
2024-12-23 22:48:57 +0100

    Init

---
+++ src/evaluation/complexity.py
@@ -0,0 +1,39 @@
+from memory_profiler import memory_usage
+import time
+from utils import format_time
+
+
+def getallparams(li):
+    params = 0
+    for module in li:
+        for param in module.parameters():
+            params += param.numel()
+    return params
+
+
+def all_in_one(trainprocess, trainmodules):
+    starttime = time.time()
+    train_losses, valid_losses = trainprocess()
+    endtime = time.time()
+
+    print("Training Time: " + format_time(endtime - starttime))
+    print("Training Params: " + str(getallparams(trainmodules)))
+    return train_losses, valid_losses
+
+
+def all_in_one_train(trainprocess, trainmodules):
+    starttime = time.time()
+    mem = max(memory_usage(proc=trainprocess))
+    endtime = time.time()
+
+    print("Training Time: " + format_time(endtime - starttime))
+    print("Training Peak Mem: " + str(mem))
+    print("Training Params: " + str(getallparams(trainmodules)))
+
+
+def all_in_one_test(testprocess, testmodules):
+    teststart = time.time()
+    testprocess()
+    testend = time.time()
+    print("Inference Time: " + format_time(testend - teststart))
+    print("Inference Params: " + str(getallparams(testmodules)))
